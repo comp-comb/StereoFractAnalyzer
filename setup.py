7f0d1e9948ebcdbcb1e38283476f1ea2d1b8f411
Khaled Mosharraf Mukut <kmmukut@gmail.com>
2024-03-02 16:54:13 -0600

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="StereoFractAnalyzer",
-    version='0.1.1',
+    version='0.1.2',
     author='Khaled Mosharraf Mukut',
     author_email='kmmukut@gmail.com',
     description='The StereoFractAnalyzer class in this package is designed for fractal dimension analysis of both 3D meshes from STL files and 2D images, incorporating functionalities for geometric transformations, visualization, and fractal dimension calculation using the box counting method. It leverages libraries like open3d, numpy, matplotlib, and PIL for processing and supports command-line arguments for user convenience.',
@@ -10,6 +10,9 @@ setup(
     long_description_content_type='text/markdown',
     url='https://github.com/kmmukut/StereoFractAnalyzer.git',
     license='MIT',
+    entry_points={
+    'console_scripts': ['StereFractAnalyzer = StereFractAnalyzer.StereFractAnalyzer:main'],
+    },
     packages=find_packages(),
     install_requires=[
         'open3d',
